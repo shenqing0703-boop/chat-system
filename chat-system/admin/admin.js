3b67496e482840525f21b03e442f02a956f7f675
shenqing0703-boop <shenqing0703@gmail.com>
2026-05-20 03:28:25 +0800

    Add files via upload

---
+++ chat-system/admin/admin.js
@@ -0,0 +1,40 @@
+let currentChat = null;
+
+db.ref("chats").on("value", snap => {
+  const data = snap.val();
+  const list = document.getElementById("list");
+
+  list.innerHTML = "";
+
+  for (let id in data) {
+    const div = document.createElement("div");
+    div.innerText = id;
+    div.onclick = () => loadChat(id);
+    list.appendChild(div);
+  }
+});
+
+function loadChat(id) {
+  currentChat = id;
+
+  document.getElementById("box").innerHTML = "";
+
+  db.ref("chats/" + id + "/messages").on("child_added", snap => {
+    const msg = snap.val();
+
+    const div = document.createElement("div");
+    div.innerText = msg.sender + ": " + msg.text;
+
+    document.getElementById("box").appendChild(div);
+  });
+}
+
+function send() {
+  const text = document.getElementById("reply").value;
+
+  db.ref("chats/" + currentChat + "/messages").push({
+    sender: "admin",
+    text: text,
+    time: Date.now()
+  });
+}
---
+++ chat-system/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDDs2uCueE3NJdZZxpMGp-1Snrt34E4nf0",
+  authDomain: "chat-system-87d17.firebaseapp.com",
+  databaseURL: "https://chat-system-87d17-default-rtdb.firebaseio.com",
+  projectId: "chat-system-87d17",
+  storageBucket: "chat-system-87d17.firebasestorage.app",
+  messagingSenderId: "988901895318",
+  appId: "1:988901895318:web:b1736c54a224570253a6b7",
+  measurementId: "G-JQZ8CCQ7TR"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
